9a01917953ad6900eb10b86b7e6dce96dc68d665
Gabriele Di Bari <dbgabri@gmail.com>
2015-04-03 11:39:43 +0200

    Added a shell menu and README in tests

--- RobotBellhop/main.cpp
+++ RobotBellhop/main.cpp
@@ -11,28 +11,62 @@
 #include "CollectionObjects.h"
 #include "ConfigFile.h"
 
-const size_t kilobytes=1024;
-const size_t megabytes=1048576;
-const size_t gigabytes=1073741824;
 
 int main(int argc, const char * argv[])
 {
+    
 #ifdef _WIN32
     //THREAD_PRIORITY_HIGHEST
     if (!SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL))
         std::cout << "Failed to set priority time critical ("<< GetLastError()<<")\n";
 #endif
-    //init alloc
+    //default file
+    std::string file_input("config.txt");
+    //else
+    if(argc > 1)
+    {
+        //arg 1 to std::string
+        std::string arg_0(argv[0]);
+        std::string arg_1(argv[1]);
+        
+        //get version
+        if(arg_1=="-v" || arg_1=="--version")
+        {
+            std::cout << "version 1.0\n";
+            return 0;
+        }
+        
+        //help
+        else if(arg_1=="-h" || arg_1=="--help")
+        {
+            std::cout << "RobotBellhop [commands]\n\n";
+            std::cout << "COMMANDS:\n";
+            std::cout << "  -h, --help     help,\n";
+            std::cout << "  -v, --version  version of the program,\n";
+            std::cout << "  <input>        input file [default is config.txt]\n";
+            return 0;
+        }
+        
+        //it is a input file
+        else
+        {
+            file_input=arg_1;
+        }
+    }
+    //init pool allocator
+    const size_t megabytes=1048576;
     RobotBellhop::ObjectsMap::init_pool_alloc(megabytes*128);
     //parsing
     std::string errors;
     RobotBellhop::ConfigFile conf_file;
-    if(!conf_file.init_from_file("config.txt",errors))
+    if(!conf_file.init_from_file(file_input,errors))
     {
         std::cout << errors;
         return -1;
     }
+    //execute
     conf_file.execute();
+    //end execute
     std::cout << "complete execution!\n";
     return 0;
 }
---
+++ Tests/README.txt
@@ -0,0 +1,20 @@
+
+- SMALL LEVEL PROBLEMS:
+	
+	Directories: Test0 end Test1
+
+- PROBLEM WITHOUT SOLUTION:
+	
+	Directory: Test2
+
+- MEDIUM LEVEL PROBLEMS:
+
+	Directories: Test 2.4, Test2.5, Test3, Test4, Test5 e Test5.5	
+
+	N.B. Test3 and Test4 there are  the same test, but the weights are changed.
+
+	N.B. In some tests the DFS solutions were not found (the execution is too long).
+
+- LARGE LEVEL PROBLEMS:
+
+	Directories: Test6, Test_cpp
